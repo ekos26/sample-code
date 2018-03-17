50d18caa4b2a86c3c5b49bf5ca6a481843f7b63f
ekos26 <elizabethkosowski@gmail.com>
2018-03-17 15:31:19 -0400

    added var

--- arraySum.js
+++ arraySum.js
@@ -1,8 +1,8 @@
 function arraySum(num){
-	sum = 0;
-	for (i = 0; i < num.length; i++){
+	var sum = 0;
+	for (var i = 0; i < num.length; i++){
 		sum = sum + num[i];
 	}
-return sum;
+	return sum;
 }
 
