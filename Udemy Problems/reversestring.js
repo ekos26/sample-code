99feb1901a0b9683eba9a1fd3825011264bc10e8
ekos26 <elizabethkosowski@gmail.com>
2018-03-21 12:16:14 -0400

    more solutions

--- Udemy Problems/reversestring.js
+++ Udemy Problems/reversestring.js
@@ -14,3 +14,31 @@ function reverse(str) {
 }
 
 module.exports = reverse;
+
+
+/* function reverse(str) {
+	return str
+	.split('')
+	.reverse()
+	.join('');
+}
+
+module.exports = reverse;
+
+*/
+
+/* function reverse(str) {
+	let reversed = '';
+
+	for (let character of str){
+		reversed = character + reversed;
+	}
+
+	return reversed;
+}
+
+module.exports = reverse;
+
+*/
+
+
